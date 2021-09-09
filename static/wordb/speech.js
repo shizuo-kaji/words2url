74d9ec31aa4226b6e0745b3d19254a87e729a120
shizuo-kaji <shizuo.kaji@gmail.com>
2021-09-09 19:15:18 +0900

    added: speech recognition

---
+++ static/wordb/speech.js
@@ -0,0 +1,31 @@
+function startConverting()
+{              
+  document.getElementById("speakBtn").style.visibility = "hidden";   
+  var r=document.getElementById('searchBox');
+  var recognition=new webkitSpeechRecognition(); //Initialisation of web Kit
+  recognition.continuous=false; //True if continous conversion is needed, false to stop transalation when paused 
+  recognition.interimResults=true;
+  recognition.lang='en-US';
+  recognition.start();
+  var ftr='';
+  recognition.onresult=function(event){
+      var interimTranscripts='';
+      for(var i=event.resultIndex;i<event.results.length;i++)
+      {
+          var transcript=event.results[i][0].transcript;
+          transcript.replace("\n"," ")
+          if(event.results[i].isFinal){
+              ftr+=transcript;
+          }
+          else
+          interimTranscripts+=transcript;
+      }
+      r.setAttribute('value',ftr +interimTranscripts) ;
+  };
+  recognition.onspeechend = function() {
+    recognition.stop();
+    document.getElementById("speakBtn").style.visibility = "visible";   
+    console.log('Speech recognition has stopped.');
+  }
+    recognition.onerror=function(event){};
+}
